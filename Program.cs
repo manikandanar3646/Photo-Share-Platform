135a1ad81898dac6e83b23ba9514d8d6fdb3df77
Manikandan AR <manikandanmnm5656@gmail.com>
2026-09-21 00:54:00 +0530

    Simplify Swagger setup in Program.cs
    Removed conditional Swagger setup for development environment.

--- Program.cs
+++ Program.cs
@@ -75,11 +75,9 @@ builder.Services.AddScoped<IGalleryService, GalleryService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 app.UseAuthentication();
 app.UseAuthorization();
