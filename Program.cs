5aaa22b3b7e42d0c413fc220a4561f496837ae1e
manikandanar3646 <manikandanmnm5656@gmail.com>
2026-09-21 06:52:55 +0530

    Allow Vercel frontend in CORS

--- Program.cs
+++ Program.cs
@@ -18,7 +18,8 @@ builder.Services.AddCors(options =>
         policy
             .WithOrigins(
                 "http://localhost:5173",
-                "http://localhost:5174"
+                "http://localhost:5174",
+                "https://photo-share-platform-g49a70932-manikandanar3646s-projects.vercel.app"
             )
             .AllowAnyHeader()
             .AllowAnyMethod();
